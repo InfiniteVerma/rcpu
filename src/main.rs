076bcf79674572b3415fa27bc02fd79a72a88073
InfiniteVerma <vermaanant25@gmail.com>
2024-06-30 22:13:26 +0530

    testing binary op in rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    let bit_true: bool = true;
+
+    let bits: u8 = 0b00000101;
+
+    let bit0 = bits & 0b00000001;
+    let bit1 = bits & 0b00000010;
+
+    println!("{}", bit0);
+    println!("{}", bit1);
 }
