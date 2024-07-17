0f4b55dbeffd97868b395be93d4611feec9c20b6
InfiniteVerma <vermaanant25@gmail.com>
2024-07-17 23:00:58 +0530

    adds a prototype dff and clock tick/tock

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,52 @@
 mod alu;
 mod gates;
+mod mem;
+use std::{thread, time};
 
-use gates::{and_gate, nand_gate, not_gate, or_gate};
+fn clock_tick(mut dff: Dff) {
+    let dff_data = [(false, false), (true, false), (false, true), (false, false)];
+    let mut i = 0;
+    loop {
+        if i > 3 {
+            i = 0;
+        }
+
+        let inp = dff_data[i].0;
+        let exp_out = dff_data[i].1;
+        thread::sleep(time::Duration::from_secs(1));
+        println!("Tick");
+        let res = dff.exec(inp);
+        println!("dff out: {}", res);
+        assert_eq!(res, exp_out);
+
+        i += 1;
+
+        let inp = dff_data[i].0;
+        let exp_out = dff_data[i].1;
+        thread::sleep(time::Duration::from_secs(1));
+        println!("Tock");
+        let res = dff.exec(inp);
+        println!("dff out: {}", res);
+        assert_eq!(res, exp_out);
+
+        i += 1;
+    }
+}
+
+#[derive(Debug)]
+struct Dff {
+    state: bool,
+}
+
+impl Dff {
+    fn exec(&mut self, input: bool) -> bool {
+        let old_state = self.state;
+        self.state = input;
+        old_state
+    }
+}
 
 fn main() {
-    println!("AND GATE: {}", and_gate(true, false));
-    println!("OR GATE: {}", or_gate(true, false));
-    println!("NOT GATE: {}", not_gate(true));
-    println!("NAND GATE: {}", nand_gate(true, false));
+    let dff = Dff { state: false };
+    clock_tick(dff);
 }
