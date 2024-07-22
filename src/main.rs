7ae1b9eeea1bfe2ce9c349fbdd5468c1bf51bc90
InfiniteVerma <vermaanant25@gmail.com>
2024-07-22 23:23:59 +0530

    clear sequential stuff for a rewrite

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,60 @@
 mod alu;
 mod gates;
 mod mem;
-use std::{thread, time};
+mod sequential;
 
-fn clock_tick(mut dff: Dff) {
-    let dff_data = [(false, false), (true, false), (false, true), (false, false)];
-    let mut i = 0;
-    loop {
-        if i > 3 {
-            i = 0;
-        }
+//fn clock_tick(mut dff: DFF) {
+    //let dff_data = [(false, false), (true, false), (false, true), (false, false)];
+    //let mut i = 0;
+    //loop {
+    //    if i > 3 {
+    //        i = 0;
+    //    }
 
-        let inp = dff_data[i].0;
-        let exp_out = dff_data[i].1;
-        thread::sleep(time::Duration::from_secs(1));
-        println!("Tick");
-        let res = dff.exec(inp);
-        println!("dff out: {}", res);
-        assert_eq!(res, exp_out);
+    //    let inp = dff_data[i].0;
+    //    let exp_out = dff_data[i].1;
+    //    thread::sleep(time::Duration::from_secs(1));
+    //    println!("Tick");
+    //    dff.clock(inp);
+    //    let res = dff.get_state();
+    //    println!("dff out: {}", res);
+    //    assert_eq!(res, exp_out);
 
-        i += 1;
+    //    i += 1;
 
-        let inp = dff_data[i].0;
-        let exp_out = dff_data[i].1;
-        thread::sleep(time::Duration::from_secs(1));
-        println!("Tock");
-        let res = dff.exec(inp);
-        println!("dff out: {}", res);
-        assert_eq!(res, exp_out);
+    //    let inp = dff_data[i].0;
+    //    let exp_out = dff_data[i].1;
+    //    thread::sleep(time::Duration::from_secs(1));
+    //    println!("Tock");
+    //    dff.clock(inp);
+    //    let res = dff.get_state();
+    //    println!("dff out: {}", res);
+    //    assert_eq!(res, exp_out);
 
-        i += 1;
-    }
-}
+    //    i += 1;
+    //}
+//}
 
-#[derive(Debug)]
-struct Dff {
-    state: bool,
-}
-
-impl Dff {
-    fn exec(&mut self, input: bool) -> bool {
-        let old_state = self.state;
-        self.state = input;
-        old_state
-    }
-}
+//#[derive(Debug)]
+//struct DFF {
+//    state: bool,
+//}
+//
+//impl DFF {
+//
+//    fn new() -> DFF {
+//        DFF { state: false }
+//    }
+//
+//    fn clock(&mut self, input: bool) {
+//        self.state = input;
+//    }
+//
+//    fn get_state(&self) -> bool {
+//        self.state
+//    }
+//}
 
 fn main() {
-    let dff = Dff { state: false };
-    clock_tick(dff);
 }
+
