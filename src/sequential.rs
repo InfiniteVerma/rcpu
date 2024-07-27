5d68b303a8b0917eeaaf0604efb43b5c38ff9206
InfiniteVerma <vermaanant25@gmail.com>
2024-07-27 19:07:34 +0530

    implement RAM8

--- src/sequential.rs
+++ src/sequential.rs
@@ -2,8 +2,10 @@ use core::panic;
 use std::thread;
 use std::time::Duration;
 
-use crate::gates::mux;
-use crate::utils::{u16_to_vec_bool, vec_bool_to_u16};
+use crate::gates::{dmux8way_gate, mux, mux8way16_gate};
+use crate::utils::{
+    u16_to_vec_bool, u8_to_vec_bool, vec_bool_to_u16, vec_bool_to_u32, vec_bool_to_u8,
+};
 
 pub struct Clock {
     is_tick: bool,
@@ -82,7 +84,8 @@ pub trait Tick {
     fn tick(&mut self);
     fn get_state(&self) -> Vec<bool>;
     //fn set_state(&mut self, state: bool);
-    fn set_state(&mut self, inputs: Vec<bool>);
+    fn set_state(&mut self, inputs: Vec<bool>); // TODO pass u64 and convert to Vec<bool> of size
+                                                // required!
 }
 
 impl DFF {
@@ -211,108 +214,250 @@ impl Tick for Register {
     }
 }
 
+/**
+ * Memory of eight 16-bit registers.
+ * If load is asserted, the value of the register selected by
+ * address is set to in; Otherwise, the value does not change.
+ * The value of the selected register is emitted by out.
+ */
+struct RAM8 {
+    registers: [Register; 8],
+    addr: u8, // 3 bits
+}
+
+impl RAM8 {
+    pub fn new() -> Self {
+        let registers = [(); 8].map(|_| Register::new());
+        RAM8 { registers, addr: 0 }
+    }
+}
+
+impl Tick for RAM8 {
+    fn tick(&mut self) {
+        for i in 0..8 {
+            self.registers[i].tick();
+        }
+    }
+
+    fn get_state(&self) -> Vec<bool> {
+        println!("get_state: self.addr: {}", self.addr);
+        let res = mux8way16_gate(
+            vec_bool_to_u16(self.registers[0].get_state()),
+            vec_bool_to_u16(self.registers[1].get_state()),
+            vec_bool_to_u16(self.registers[2].get_state()),
+            vec_bool_to_u16(self.registers[3].get_state()),
+            vec_bool_to_u16(self.registers[4].get_state()),
+            vec_bool_to_u16(self.registers[5].get_state()),
+            vec_bool_to_u16(self.registers[6].get_state()),
+            vec_bool_to_u16(self.registers[7].get_state()),
+            self.addr,
+        );
+
+        u16_to_vec_bool(res)
+    }
+
+    fn set_state(&mut self, inputs: Vec<bool>) {
+        // 16 + 1 + 3
+        assert_eq!(inputs.len(), 16 + 1 + 3);
+
+        let load = inputs[16];
+        let mut addr: Vec<bool> = vec![false; 3];
+
+        for i in 0..3 {
+            addr[i] = inputs[i + 17];
+        }
+
+        self.addr = vec_bool_to_u8(addr.clone());
+
+        println!(
+            "ANANT load: {}, addr: {:#?}, self.addr: {:#?}",
+            load, addr, self.addr
+        );
+
+        let dmux_out: Vec<bool> = u8_to_vec_bool(dmux8way_gate(load, vec_bool_to_u8(addr)));
+
+        /*
+         * Input is vector of bool. Here converting to u32
+         *  - Then right shift 4 and take the 16 LSB bits as input data
+         */
+        let input_bit: u32 = vec_bool_to_u32(inputs.clone()) >> 4;
+        let inp_bool = u16_to_vec_bool((input_bit & 0xFFFF) as u16);
+
+        assert_eq!(inp_bool.len(), 16);
+
+        for i in 0..8 {
+            let mut input = inp_bool.clone();
+            input.extend([dmux_out[i]]);
+            self.registers[i].set_state(input);
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use crate::utils::u32_to_vec_bool;
+
     use super::*;
 
+    //#[test]
+    //fn test_dff() {
+    //    let mut clock = Clock::new();
+
+    //    let dff1 = DFF::new();
+
+    //    clock.register(Box::new(dff1));
+    //    clock.set_state_of_component(0, vec![true]);
+    //    let out = clock.get_state_of_component(0);
+    //    assert_eq!(out.len(), 1);
+    //    assert_eq!(out[0], false);
+
+    //    clock.tick();
+    //    let out = clock.get_state_of_component(0);
+    //    assert_eq!(out.len(), 1);
+    //    assert_eq!(out[0], true);
+
+    //    clock.set_state_of_component(0, vec![false]);
+    //    let out = clock.get_state_of_component(0);
+    //    assert_eq!(out.len(), 1);
+    //    assert_eq!(out[0], true);
+
+    //    clock.tick();
+    //    let out = clock.get_state_of_component(0);
+    //    assert_eq!(out.len(), 1);
+    //    assert_eq!(out[0], false);
+    //}
+
+    //#[test]
+    //fn test_bit() {
+    //    let mut clock = Clock::new();
+
+    //    let bit1 = Bit::new();
+
+    //    clock.register(Box::new(bit1));
+    //    let out = clock.get_state_of_component(0);
+    //    assert_eq!(out.len(), 1);
+    //    assert_eq!(out[0], false);
+
+    //    clock.set_state_of_component(0, vec![false, false]);
+
+    //    clock.tick();
+    //    let out = clock.get_state_of_component(0);
+    //    assert_eq!(out.len(), 1);
+    //    assert_eq!(out[0], false);
+
+    //    clock.set_state_of_component(0, vec![true, false]);
+
+    //    clock.tick();
+    //    let out = clock.get_state_of_component(0);
+    //    assert_eq!(out.len(), 1);
+    //    assert_eq!(out[0], false);
+
+    //    clock.set_state_of_component(0, vec![true, true]);
+
+    //    clock.tick();
+    //    let out = clock.get_state_of_component(0);
+    //    assert_eq!(out.len(), 1);
+    //    assert_eq!(out[0], true);
+
+    //    clock.set_state_of_component(0, vec![false, false]);
+
+    //    clock.tick();
+    //    let out = clock.get_state_of_component(0);
+    //    assert_eq!(out.len(), 1);
+    //    assert_eq!(out[0], true);
+    //}
+
+    //#[test]
+    //fn test_register() {
+    //    let mut clock = Clock::new();
+
+    //    let reg1 = Register::new();
+
+    //    clock.register(Box::new(reg1));
+    //    let out = clock.get_state_of_component(0);
+    //    assert_eq!(out.len(), 16);
+    //    assert_eq!(out, [false; 16]);
+
+    //    let inp_bool: u16 = 0b1000000010000000;
+    //    let mut inp = u16_to_vec_bool(inp_bool);
+    //    inp.extend([true]);
+    //    clock.set_state_of_component(0, inp);
+    //    clock.tick();
+
+    //    let out = clock.get_state_of_component(0);
+    //    assert_eq!(out.len(), 16);
+    //    assert_eq!(vec_bool_to_u16(out), inp_bool);
+
+    //    let inp_bool2: u16 = 0b1000000010000001;
+    //    let mut inp2 = u16_to_vec_bool(inp_bool2);
+    //    inp2.extend([false]);
+    //    clock.set_state_of_component(0, inp2);
+    //    clock.tick();
+
+    //    let out = clock.get_state_of_component(0);
+    //    assert_eq!(out.len(), 16);
+    //    assert_eq!(vec_bool_to_u16(out), inp_bool);
+    //}
+
     #[test]
-    fn test_dff() {
+    fn test_ram8() {
         let mut clock = Clock::new();
 
-        let dff1 = DFF::new();
+        let ram8 = RAM8::new();
 
-        clock.register(Box::new(dff1));
-        clock.set_state_of_component(0, vec![true]);
-        let out = clock.get_state_of_component(0);
-        assert_eq!(out.len(), 1);
-        assert_eq!(out[0], false);
-
-        clock.tick();
-        let out = clock.get_state_of_component(0);
-        assert_eq!(out.len(), 1);
-        assert_eq!(out[0], true);
-
-        clock.set_state_of_component(0, vec![false]);
-        let out = clock.get_state_of_component(0);
-        assert_eq!(out.len(), 1);
-        assert_eq!(out[0], true);
-
-        clock.tick();
-        let out = clock.get_state_of_component(0);
-        assert_eq!(out.len(), 1);
-        assert_eq!(out[0], false);
-    }
-
-    #[test]
-    fn test_bit() {
-        let mut clock = Clock::new();
-
-        let bit1 = Bit::new();
-
-        clock.register(Box::new(bit1));
-        let out = clock.get_state_of_component(0);
-        assert_eq!(out.len(), 1);
-        assert_eq!(out[0], false);
-
-        clock.set_state_of_component(0, vec![false, false]);
-
-        clock.tick();
-        let out = clock.get_state_of_component(0);
-        assert_eq!(out.len(), 1);
-        assert_eq!(out[0], false);
-
-        clock.set_state_of_component(0, vec![true, false]);
-
-        clock.tick();
-        let out = clock.get_state_of_component(0);
-        assert_eq!(out.len(), 1);
-        assert_eq!(out[0], false);
-
-        clock.set_state_of_component(0, vec![true, true]);
-
-        clock.tick();
-        let out = clock.get_state_of_component(0);
-        assert_eq!(out.len(), 1);
-        assert_eq!(out[0], true);
-
-        clock.set_state_of_component(0, vec![false, false]);
-
-        clock.tick();
-        let out = clock.get_state_of_component(0);
-        assert_eq!(out.len(), 1);
-        assert_eq!(out[0], true);
-    }
-
-    #[test]
-    fn test_register() {
-        assert_eq!(1, 1);
-        let mut clock = Clock::new();
-
-        let reg1 = Register::new();
-
-        clock.register(Box::new(reg1));
+        clock.register(Box::new(ram8));
         let out = clock.get_state_of_component(0);
         assert_eq!(out.len(), 16);
         assert_eq!(out, [false; 16]);
 
-        let inp_bool: u16 = 0b1000000010000000;
-        let mut inp = u16_to_vec_bool(inp_bool);
-        inp.extend([true]);
+        /*
+         * Test 1: Set and get register at addr 010
+         */
+        let input: u32 = 0b1000000011111111;
+        let load = 1;
+        let addr = 0b101;
+        let mut final_input: u32 = input;
+        final_input = (final_input << 1) | load;
+        final_input = (final_input << 3) | addr;
+
+        let inp = u32_to_vec_bool(final_input)[12..].to_vec();
         clock.set_state_of_component(0, inp);
         clock.tick();
 
         let out = clock.get_state_of_component(0);
         assert_eq!(out.len(), 16);
-        assert_eq!(vec_bool_to_u16(out), inp_bool);
+        assert_eq!(vec_bool_to_u16(out), (input & 0xFFFF) as u16);
 
-        let inp_bool2: u16 = 0b1000000010000001;
-        let mut inp2 = u16_to_vec_bool(inp_bool2);
-        inp2.extend([false]);
-        clock.set_state_of_component(0, inp2);
+        /*
+         * Test 2: Read it after another tick, load ==0 at same addr
+         */
+        let input: u32 = 0;
+        let load = 0;
+        let addr = 0b101;
+        let final_input: u32 = (((input << 1) | load) << 3) | addr;
+
+        let inp = u32_to_vec_bool(final_input)[12..].to_vec();
+        clock.set_state_of_component(0, inp);
         clock.tick();
 
         let out = clock.get_state_of_component(0);
         assert_eq!(out.len(), 16);
-        assert_eq!(vec_bool_to_u16(out), inp_bool);
+        assert_eq!(vec_bool_to_u16(out), 0b1000000011111111);
+
+        /*
+         * Test 3: Read a different addr
+         */
+        let input: u32 = 0;
+        let load = 0;
+        let addr = 0b100;
+        let final_input: u32 = (((input << 1) | load) << 3) | addr;
+
+        let inp = u32_to_vec_bool(final_input)[12..].to_vec();
+        clock.set_state_of_component(0, inp);
+        clock.tick();
+
+        let out = clock.get_state_of_component(0);
+        assert_eq!(out.len(), 16);
+        assert_eq!(vec_bool_to_u16(out), 0);
     }
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,19 +1,151 @@
 pub fn u16_to_vec_bool(value: u16) -> Vec<bool> {
-    (0..16).map(|i| (value & (1 << i)) != 0).collect()
+    let mut res: Vec<bool> = (0..16).map(|i| (value & (1 << i)) != 0).collect();
+    res.reverse();
+    res
 }
 
-pub fn vec_bool_to_u16(value: Vec<bool>) -> u16 {
-    assert_eq!(value.len(), 16);
-
+pub fn vec_bool_to_u16(mut value: Vec<bool>) -> u16 {
     let mut res: u16 = 0;
+    value.reverse();
     for i in 0..value.len() {
         let bit = match value[i] {
             false => 0,
             true => 1,
         };
 
-        res |= (bit << i);
+        res |= bit << i;
     }
 
     res
+}
+
+pub fn vec_bool_to_u8(mut value: Vec<bool>) -> u8 {
+    let mut res: u8 = 0;
+
+    value.reverse();
+
+    for i in 0..value.len() {
+        let bit = match value[i] {
+            false => 0,
+            true => 1,
+        };
+
+        res |= bit << i;
+    }
+
+    res
+}
+
+pub fn u8_to_vec_bool(value: u8) -> Vec<bool> {
+    let mut res: Vec<bool> = (0..8).map(|i| (value & (1 << i)) != 0).collect();
+    res.reverse();
+    res
+}
+
+pub fn u32_to_vec_bool(value: u32) -> Vec<bool> {
+    let mut res: Vec<bool> = (0..32).map(|i| (value & (1 << i)) != 0).collect();
+    res.reverse();
+    res
+}
+
+pub fn vec_bool_to_u32(mut value: Vec<bool>) -> u32 {
+    let mut res: u32 = 0;
+
+    value.reverse();
+
+    for i in 0..value.len() {
+        let bit = match value[i] {
+            false => 0,
+            true => 1,
+        };
+
+        res |= bit << i;
+    }
+
+    res
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::utils::{
+        u16_to_vec_bool, u32_to_vec_bool, vec_bool_to_u16, vec_bool_to_u32, vec_bool_to_u8,
+    };
+
+    #[test]
+    fn test_u16_to_vec_bool() {
+        let inp: u16 = 0b1;
+        let mut exp_out = vec![false; 16];
+        exp_out[15] = true;
+
+        assert_eq!(u16_to_vec_bool(inp), exp_out);
+
+        let inp: u16 = 0b1111111111111111;
+        let exp_out = vec![true; 16];
+
+        assert_eq!(u16_to_vec_bool(inp), exp_out);
+    }
+
+    #[test]
+    fn test_vec_bool_to_u16() {
+        let inp = vec![true];
+        let exp_out: u16 = 0b1;
+
+        assert_eq!(vec_bool_to_u16(inp), exp_out);
+
+        let inp = vec![true, false, true, false];
+        let exp_out: u16 = 0b1010;
+
+        assert_eq!(vec_bool_to_u16(inp), exp_out);
+    }
+
+    #[test]
+    fn test_vec_bool_to_u8() {
+        let inp = vec![true];
+        let exp_out: u8 = 0b1;
+
+        assert_eq!(vec_bool_to_u8(inp), exp_out);
+
+        let inp = vec![true, false, false, false, true];
+        let exp_out: u8 = 0b10001;
+
+        assert_eq!(vec_bool_to_u8(inp), exp_out);
+
+        let inp = vec![true, false, false, true, true];
+        let exp_out: u8 = 0b10011;
+
+        assert_eq!(vec_bool_to_u8(inp), exp_out);
+
+        let inp = vec![true, false, true, true];
+        let exp_out: u8 = 0b1011;
+
+        assert_eq!(vec_bool_to_u8(inp), exp_out);
+    }
+
+    #[test]
+    fn test_u32_to_vec_bool() {
+        let inp: u32 = 0b1;
+        let mut exp_out = vec![false; 32];
+        exp_out[31] = true;
+
+        assert_eq!(u32_to_vec_bool(inp), exp_out);
+
+        let mut inp: u32 = 0b1111111111111111;
+        inp = inp << 16 | 0b1111111111111111;
+        let exp_out = vec![true; 32];
+
+        assert_eq!(u32_to_vec_bool(inp), exp_out);
+    }
+
+    #[test]
+    fn test_vec_bool_to_u32() {
+        let inp = vec![true];
+        let exp_out: u32 = 0b1;
+
+        assert_eq!(vec_bool_to_u32(inp), exp_out);
+
+        let inp = vec![true, false, false, false, true];
+        let exp_out: u32 = 0b10001;
+
+        assert_eq!(vec_bool_to_u32(inp), exp_out);
+    }
 }
